379c62bdfba32b393c9c5a6e57698de8ef2b7d15
Antony Joe <antony.joe@factweavers.com>
2022-11-17 15:15:57 +0530

    changes

--- src/main/java/simple/AwsSqsClient.java
+++ src/main/java/simple/AwsSqsClient.java
@@ -1,4 +1,4 @@
-package client;
+package simple;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
--- src/main/java/simple/Consumer.java
+++ src/main/java/simple/Consumer.java
@@ -1,6 +1,5 @@
 package simple;
 
-import client.AwsSqsClient;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.Message;
@@ -12,22 +11,7 @@ public class Consumer {
     public static void main(String[] args) {
         try{
             AmazonSQS sqs = AwsSqsClient.getAwsSqsClient();
-            while(true){
-                List<Message> messages = sqs.receiveMessage(AwsSqsClient.getQueueUrl()).getMessages();
-                if(messages.size() == 0){
-                    System.out.println("thread sleeping");
-                    Thread.sleep(5000);
-                }
-                else{
-                    messages.forEach(x -> {
-                        System.out.println("message >> "+x.getBody());
-                        DeleteMessageRequest dr = new DeleteMessageRequest();
-                        dr.setQueueUrl(AwsSqsClient.getQueueUrl());
-                        dr.setReceiptHandle(x.getReceiptHandle());
-                        sqs.deleteMessage(dr);
-                    });
-                }
-            }
+
         }catch (Exception e){
             System.out.println("error - " +e.getMessage());
         }
